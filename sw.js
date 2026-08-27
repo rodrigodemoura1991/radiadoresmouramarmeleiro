30cee69405bde1c33c66f821a68adbd3f91f1b50
rodrigodemoura1991 <4g8by9zvw7@privaterelay.appleid.com>
2026-08-27 05:10:14 -0300

    Atualizar cache do PWA para aplicar correção do cabeçalho mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'radiadores-moura-v1';
+const CACHE_NAME = 'radiadores-moura-v2-20260827-0515';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
