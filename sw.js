bc1f9d5a371ecf4c5e9693ccf77cc9c071b8872a
rodrigodemoura1991 <4g8by9zvw7@privaterelay.appleid.com>
2026-09-01 12:03:36 -0300

    Desativar service worker antigo e limpar cache

--- sw.js
+++ sw.js
@@ -1,29 +1,14 @@
-const CACHE_NAME = 'radiadores-moura-v6-20260901-0800';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest'];
-
+// Service Worker desativado durante a estabilização do aplicativo.
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(client => client.navigate(client.url)))
+      .then(() => self.registration.unregister())
   );
 });
