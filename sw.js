3437b172ae82a5a1b2eb11989c4f430c678a93fa
rodrigodemoura1991 <4g8by9zvw7@privaterelay.appleid.com>
2026-09-08 10:20:56 -0300

    Atualizar cache do app para incluir relatório PDF

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'radiadores-moura-v43-popup-fix';
+const CACHE_NAME = 'radiadores-moura-v44-pdf';
 const APP_SHELL = [
   './',
   './index.html',
@@ -15,6 +15,7 @@ const APP_SHELL = [
   './supabase-config.js?v=stable5',
   './js/launch-reference-test.js?v=v30-popup',
   './js/servicos-popup-edicao.js?v=v30-popup',
+  './js/servicos-pdf.js?v=pdf1',
   './assets/logo-radiadores-moura.svg',
   './manifest.webmanifest',
   './8857A320-4E57-4A00-933D-C76434BC6953.png'
